44a7d3d8dbbce6878a44456a53060cf19039ba7e
129duckflew <129duckflew@gmail.com>
2021-08-20 22:21:48 +0800

    初始化项目写到了UserDetails还没有些过滤器

---
+++ yeb-server/src/main/java/cn/duckflew/controller/MailLogController.java
@@ -0,0 +1,19 @@
+package cn.duckflew.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author duckflew
+ * @since 2021-03-24
+ */
+@RestController
+@RequestMapping("/mail-log")
+public class MailLogController {
+
+}
---
+++ yeb-server/src/main/java/cn/duckflew/service/impl/MenuRoleServiceImpl.java
@@ -0,0 +1,20 @@
+package cn.duckflew.service.impl;
+
+import cn.duckflew.mapper.MenuRoleMapper;
+import cn.duckflew.pojo.MenuRole;
+import cn.duckflew.service.IMenuRoleService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author duckflew
+ * @since 2021-03-24
+ */
+@Service
+public class MenuRoleServiceImpl extends ServiceImpl<MenuRoleMapper, MenuRole> implements IMenuRoleService {
+
+}
