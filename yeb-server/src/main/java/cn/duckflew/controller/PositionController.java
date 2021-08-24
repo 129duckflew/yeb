12f1f402a5d8ac260be4522bbd7f4e1444a009c1
duckflew <129duckflew@gmail.com>
2021-08-24 23:41:54 +0800

    添加职位管理

--- yeb-server/src/main/java/cn/duckflew/controller/PositionController.java
+++ yeb-server/src/main/java/cn/duckflew/controller/PositionController.java
@@ -1,8 +1,16 @@
 package cn.duckflew.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import cn.duckflew.pojo.Position;
+import cn.duckflew.pojo.RespBean;
+import cn.duckflew.service.IPositionService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * <p>
@@ -16,4 +24,45 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/position")
 public class PositionController {
 
+    @Autowired
+    IPositionService positionService;
+    @ApiOperation(value = "获取所有职位")
+    @GetMapping("/")
+    public List<Position> getAllPosition()
+    {
+        return positionService.list();
+    }
+
+    @ApiOperation(value = "添加职位")
+    @PostMapping("/")
+    public RespBean addPosition(@RequestBody  Position position)
+    {
+        position.setCreateDate(LocalDateTime.now());
+        boolean save = positionService.save(position);
+        if (save)return RespBean.success("添加成功");
+        return RespBean.success("添加失败");
+
+    }
+    @ApiOperation(value = "更新职位")
+    @PutMapping("/")
+    public RespBean updatePoistion(Position position)
+    {
+        boolean success = positionService.updateById(position);
+        if (success)
+        {
+            return RespBean.success("更新成功");
+        }
+        return RespBean.error("更新失败");
+    }
+    @ApiOperation(value = "删除职位")
+    @DeleteMapping("/{id}")
+    public RespBean deletePosition(@PathVariable Integer id)
+    {
+        boolean success = positionService.removeById(id);
+        if (success)
+        {
+            return RespBean.success("删除成功");
+        }
+        return RespBean.error("删除失败");
+    }
 }
